5ed93009bc5a1fe28d243d56cc48838b3edd9914
EmanEjaz97 <51271816+EmanEjaz97@users.noreply.github.com>
2019-08-17 00:42:45 +0500

    change the auth token error settings

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -14,7 +14,7 @@ const auth = async (req, res, next) => {
     req.user = user;
     next();
   } catch (error) {
-    res.status(401).send({ error: "Pls authenticate!" });
+    res.status(401).send(req.user);
   }
 };
 module.exports = auth;
